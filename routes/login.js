75b708f8bac7d0a3c5be79d093c33cd7c7135da7
Felix Klauke <info@felix-klauke.de>
2017-10-24 12:47:15 +0200

    Feature/authentication (#12)
    * Install passport.
    
    * Change navbar to include user login state stub.

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Caprica', currentPage: 'login' });
 });
+
+router.post('/',
+    passport.authenticate('local'),
+    function (req, res) {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        res.redirect('/users/' + req.user.username);
+    });
 
 module.exports = router;
